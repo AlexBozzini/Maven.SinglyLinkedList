8aada880661fa0d9dcaf9b828ed5b672d0d79fd8
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-15 21:57:48 -0400

    Finished add/remove/contains

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,21 +1,25 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import javax.xml.bind.Element;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList<T> {
+public class SinglyLinkedList {
     Node head;
 
-    static class Node<T>{
-        T data;
+    public static class Node{
+        int data;
         Node next;
-        Node(T d){
+        Node(int d){
             data = d;
             next = null;
         }
     }
 
-    public static SinglyLinkedList add(SinglyLinkedList list, T data){
+    public SinglyLinkedList add(SinglyLinkedList list, int data){
         Node new_node = new Node(data);
         new_node.next = null;
 
@@ -29,5 +33,54 @@ public class SinglyLinkedList<T> {
             last.next = new_node;
         }
         return list;
+    }
+
+    public SinglyLinkedList remove(SinglyLinkedList list, int index){
+        Node currNode = list.head, prev = null;
+        if (index == 0 && currNode != null){
+            list.head = currNode.next;
+            System.out.println(index + " position element deleted");
+            return list;
+        }
+        int counter = 0;
+        while (currNode != null){
+            if (counter == index){
+                prev.next = currNode.next;
+                System.out.println(index + " position element deleted");
+                break;
+            } else {
+                prev = currNode;
+                currNode = currNode.next;
+                counter++;
+            }
+        }
+        if (currNode == null){
+            System.out.println(index + " position element not found");
+        }
+        return list;
+    }
+
+    public boolean contains(SinglyLinkedList list, int data){
+        Node currNode = list.head;
+        while (currNode != null){
+            if (currNode.data == data){
+                return true;
+            } else {
+                currNode = currNode.next;
+            }
+        }
+        return false;
+    }
+
+    public static void printList(SinglyLinkedList list){
+        Node currNode = list.head;
+
+        System.out.println("\nLinkedList: ");
+
+        while (currNode != null){
+            System.out.println(currNode.data + " ");
+
+            currNode = currNode.next;
+        }
     }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,58 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void addTest(){
+        // given
+        SinglyLinkedList list = new SinglyLinkedList();
+        int data = 5;
+        // when
+        list.add(list, data);
+        // then
+        Assert.assertTrue(list.contains(list, data));
+    }
+
+    @Test
+    public void removeTest(){
+        // given
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(list, 39);
+        list.add(list, 5);
+        list.add(list, -8);
+        // when
+        list.remove(list, 0);
+        // then
+        Assert.assertFalse(list.contains(list, 39));
+    }
+
+    @Test
+    public void removeTest2(){
+        // given
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(list, 39);
+        list.add(list, 5);
+        list.add(list, -8);
+        // when
+        list.remove(list, 2);
+        // then
+        Assert.assertFalse(list.contains(list, -8));
+    }
+
+    @Test
+    public void containsTest(){
+        // given
+        SinglyLinkedList list = new SinglyLinkedList();
+        // when
+        list.add(list, -23);
+        list.add(list, 82);
+        list.add(list, 235);
+        // then
+        Assert.assertTrue(list.contains(list, 82));
+    }
 }
