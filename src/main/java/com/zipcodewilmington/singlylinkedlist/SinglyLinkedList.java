13fe1b3117b67b4a10ddc76b2d16ee653d1e56fd
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-15 20:47:35 -0400

    Initial commit

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,31 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    Node head;
+
+    static class Node<T>{
+        T data;
+        Node next;
+        Node(T d){
+            data = d;
+            next = null;
+        }
+    }
+
+    public static SinglyLinkedList add(SinglyLinkedList list, T data){
+        Node new_node = new Node(data);
+        new_node.next = null;
+
+        if (list.head == null){
+            list.head = new_node;
+        } else {
+            Node last = list.head;
+            while (last.next != null){
+                last = last.next;
+            }
+            last.next = new_node;
+        }
+        return list;
+    }
 }
